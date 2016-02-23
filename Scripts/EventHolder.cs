40c2f18d209b13a492fe92c4ecb9e6753f7061f4
Aram Joseph <ajoseph@IT304.mr.preble.com>
2016-02-23 10:08:40 -0500

    Stuff I'm working on.
    Hopefully it runs correctly.

---
+++ Scripts/EventHolder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHolder : MonoBehaviour 
+{
+	PlayerController pl;
+
+	void Start ()
+	{
+		pl = transform.root.GetComponent<PlayerController> ();
+	}
+
+	public void ThrowProjectile()
+	{
+		pl.specialAttack = true;
+	}
+}
